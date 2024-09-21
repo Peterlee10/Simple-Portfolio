eb97cc84c9b4bc05c05f1abd9161dc09261668a3
Peterlee10 <peterogundipe84@gmail.com>
2024-09-21 12:59:09 +0100

    My Simple Portfolio

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    const message = document.getElementById('message');
+    message.innerHTML = 'Feel free to contact me at: peterogundipe84@gmail.com or +234 812 632 8583';
+}
